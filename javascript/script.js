33e78ab9b88a6994e1c10921656b9f29effa7444
Moahmed Gassama <bouyagassama@gmail.com>
2016-02-29 02:22:06 -0600

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+function mySelf() {
+              	var x = document.getElementById('id');
+              	 var y = document.getElementById('k');
+              	y.style.fontSize = "24px";
+              	x.style.color = "pink";
+              	y.style.color = "pink";
+ }
+                
+  {
+  	 var intro = document.getElementById('la');
+  	intro.innerHTML = "bye";
+
+
+  }
+
